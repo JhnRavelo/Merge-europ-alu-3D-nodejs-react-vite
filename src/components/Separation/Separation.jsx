3b9da784cadcacb01a1b2aef0f9ec9da6b36b875
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-06 10:01:23 +0300

    ajout composant separation

---
+++ src/components/Separation/Separation.jsx
@@ -0,0 +1,15 @@
+import './Separation.css'
+
+const Separation = () => {
+  return (
+    <>
+        <div className="header__produits__fenetre">
+            <div className="heading-section">
+                <span className="subheading">X</span>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Separation
