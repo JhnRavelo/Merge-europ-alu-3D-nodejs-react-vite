07569da13dd5b43394124be412aebcead6b002d1
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-07 14:09:05 +0300

    ajput de ScrollToTop component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import Chemins from '../../routers/Chemins'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Grids from '../Grids/Grids'
+import ScrollToTop from '../ScrollToTop/ScrollToTop'
 
 const Layout = () => {
   return (
     <div>
+      <ScrollToTop/>
       <Header/>
       <div>
         <Grids/>
