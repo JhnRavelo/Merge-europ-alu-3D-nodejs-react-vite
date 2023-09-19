4eeeb5958ecbcb7c186f7e494c07c05b7e7075f4
JhnRavelo <johnravelo135@gmail.com>
2023-09-19 17:01:12 +0300

    update page and delete product

--- server/controllers/pageController.js
+++ server/controllers/pageController.js
@@ -1,7 +1,8 @@
 const { pages } = require("../database/models");
 
 const addPage = async (req, res) => {
-  const { page, icon, position, minYAngle, maxYAngle, minXAngle, maxXAngle } = await req.body;
+  const { page, icon, position, minYAngle, maxYAngle, minXAngle, maxXAngle } =
+    await req.body;
   if (page) {
     const result = await pages.create({
       page,
@@ -13,6 +14,40 @@ const addPage = async (req, res) => {
       maxXAngle,
     });
 
+    if (result) {
+      res.json("Page ajouté");
+    }
+  }
+};
+
+const updatePage = async (req, res) => {
+  const {
+    id,
+    page,
+    icon,
+    position,
+    minYAngle,
+    maxYAngle,
+    minXAngle,
+    maxXAngle,
+  } = await req.body;
+  if (id) {
+    const updatePage = await findOne({
+      where: {
+        ID_page: id,
+      },
+    });
+
+    updatePage.set(
+      page,
+      icon,
+      position,
+      minXAngle,
+      minYAngle,
+      maxXAngle,
+      maxYAngle
+    );
+    const result = await updatePage.save();
     if (result) {
       res.json("Page ajouté");
     }
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,13 +4,8 @@ const products = require("../database/models/products");
 const addProduct = async (req, res) => {
   const { page, title, description, png } = await req.body;
 
-  if (page && title && description && png ) {
-    const result = await products.create(
-      page,
-      title,
-      description,
-      png,
-    );
+  if (page && title && description && png) {
+    const result = await products.create(page, title, description, png);
 
     if (result) {
       res.json("Produit ajouté");
@@ -19,28 +14,52 @@ const addProduct = async (req, res) => {
 };
 
 const updateProduct = async (req, res) => {
-    const {id ,page, title, description, png} = await req.body
+  const { id, page, title, description, png } = await req.body;
 
-    if(id){
-        const product = await products.findOne({
-            where: {
-                ID_product: id
-            }
-        })
+  if (id) {
+    const product = await products.findOne({
+      where: {
+        ID_product: id,
+      },
+    });
 
-        product.page = page
-        product.title = title
-        product.description = description
-        product.png = png
+    product.set(
+        page,
+        title,
+        description,
+        png,
+    )
 
-        await product.save()
+    const result = await product.save();
+
+    if (result) {
+      res.json("Produit modifié");
     }
-}
+  }
+};
 
-const readproduct = async (req, res) => {
-    const products = await products.findAll()
+const readProduct = async (req, res) => {
+  const products = await products.findAll();
 
-    res.send(products)
-}
+  if (products) {
+    res.send(products);
+  }
+};
 
-module.exports = { addProduct, updateProduct, readproduct };
+const deleteProduct = async (req, res) => {
+  const { id } = await req.body;
+
+  if (id) {
+    const result = await products.destroy({
+      where: {
+        ID_product: id,
+      },
+    });
+
+    if (result) {
+      res.json("Produit supprimé");
+    }
+  }
+};
+
+module.exports = { addProduct, updateProduct, readProduct, deleteProduct };
