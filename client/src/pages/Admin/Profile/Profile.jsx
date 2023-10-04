0e34ecb3ee1156ca80ed5dab41d34dadd9fe6251
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-04 10:41:48 +0300

    vita profil admin

--- client/src/pages/Admin/Profile/Profile.jsx
+++ client/src/pages/Admin/Profile/Profile.jsx
@@ -1,9 +1,36 @@
-import "./Profile.scss"
+import "./Profile.scss";
+import AdminPhoto from "/admin.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div id="profile">
+      <h1>Profile</h1>
+      <div className="edit__admin">
+        <h2>Modifier votre profile</h2>
+        <button className="edit__profile">
+          <FontAwesomeIcon icon={faEdit} beat />
+          éditer
+        </button>
+      </div>
+      <div className="admin__card">
+        <div className="admin__img">
+          <img src={AdminPhoto} alt="sary" />
+        </div>
+        <div className="info__admin">
+          <h1 className="admin__name">Sylvestre Hardy</h1>
+          <h2 className="admin__email">hardy@gmail.com</h2>
+          <h1 className="user__admin">- Administrateur</h1>
+        </div>
+      </div>
+      <button className="logout__button">
+        <FontAwesomeIcon icon={faSignOutAlt} flip />
+        Se déconnecter
+      </button>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
