57cfed1de451647f8021a6e22731ffe4787ad12d
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-05 17:10:38 +0300

    journal component

---
+++ client/src/pages/Admin/Log/Log.jsx
@@ -0,0 +1,57 @@
+import "./Log.scss";
+
+const Log = () => {
+  return (
+    <>
+      <h1>Journals</h1>
+      <div className="log">
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+        <div className="journal">
+          <h2>{"L'utilisateur zao a zao de nanao an zao de nanao an zao"}</h2>
+          <h2 className="date">23/34/3553</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Log;
