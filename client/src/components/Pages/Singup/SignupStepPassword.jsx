c3e0b1be4a0bfc27467139604537a23f24cf4392
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-06 10:31:31 +0300

    signup modification

--- client/src/components/Pages/Singup/SignupStepPassword.jsx
+++ client/src/components/Pages/Singup/SignupStepPassword.jsx
@@ -1,43 +1,74 @@
-import { ErrorMessage, Field } from 'formik';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faKey } from '@fortawesome/free-solid-svg-icons';
+import { ErrorMessage, Field } from "formik";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
 
 const SignupStepPassword = () => {
+  const [visiblePass, setVisiblePass] = useState(false);
+  const [visibleConfirm, setVisibleConfirm] = useState(false);
+
   return (
     <>
-      <div className='fields'>
-        <div className='username'>
-          <FontAwesomeIcon icon={faKey} className='fa fa-user' />
+      <div className="fields">
+        <div className="username">
+          <FontAwesomeIcon icon={faKey} className="fa fa-user" />
           <Field
-            type='password'
-            className='user-input'
-            name='password'
-            autoComplete='off'
+            type={visiblePass ? "text" : "password"}
+            className="user-input"
+            name="password"
+            autoComplete="off"
             required
-            placeholder='Mot de passe'
+            placeholder="Mot de passe"
           />
+          {visiblePass ? (
+            <AiOutlineEye
+              className="absolute right-6 top-10 cursor-pointer"
+              size={25}
+              onClick={() => setVisiblePass(false)}
+            />
+          ) : (
+            <AiOutlineEyeInvisible
+              className="absolute right-6 top-10 cursor-pointer"
+              size={25}
+              onClick={() => setVisiblePass(true)}
+            />
+          )}
         </div>
         <ErrorMessage
-          className='error name-error'
-          name='password'
-          component={'p'}
+          className="error name-error"
+          name="password"
+          component={"p"}
         />
-        <div className='username' style={{ marginTop: '15px' }}>
-          <FontAwesomeIcon icon={faKey} className='fa fa-user' />
+        <div className="username" style={{ marginTop: "15px" }}>
+          <FontAwesomeIcon icon={faKey} className="fa fa-user" />
           <Field
-            type='password'
-            className='user-input'
-            name='confirmPassword'
-            autoComplete='off'
+            type={visibleConfirm ? "text" : "password"}
+            className="user-input"
+            name="confirmPassword"
+            autoComplete="off"
             required
-            inputMode='password'
-            placeholder='Confirmez votre mot de passe'
+            inputMode="password"
+            placeholder="Confirmez votre mot de passe"
           />
+          {visibleConfirm ? (
+            <AiOutlineEye
+              className="absolute right-6 top-10 cursor-pointer"
+              size={25}
+              onClick={() => setVisibleConfirm(false)}
+            />
+          ) : (
+            <AiOutlineEyeInvisible
+              className="absolute right-6 top-10 cursor-pointer"
+              size={25}
+              onClick={() => setVisibleConfirm(true)}
+            />
+          )}
         </div>
         <ErrorMessage
-          className='error name-error'
-          name='confirmPassword'
-          component={'p'}
+          className="error name-error"
+          name="confirmPassword"
+          component={"p"}
         />
       </div>
     </>
