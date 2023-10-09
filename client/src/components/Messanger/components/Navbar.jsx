dd713b7450b4eb9c94e9b2dac3b3dc2bcd980ce3
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-09 17:07:16 +0300

    search

--- client/src/components/Messanger/components/Navbar.jsx
+++ client/src/components/Messanger/components/Navbar.jsx
@@ -1,32 +1,62 @@
-import React, { useContext } from 'react'
-// import photos from "../img/add.png"
-import { signOut } from 'firebase/auth'
-import { auth } from '../firebase'
-import { AuthContext } from '../context/AuthContext'
-import Xmark from '../img/x.png'
+import Xmark from "../img/x.png";
+import userPhoto from "../img/avatar/homme.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
-const Navbar = () => { 
-  const {currentUser} = useContext(AuthContext)
-
+const Navbar = () => {
   const handleOpenMenu = () => {
-    const sidebar = document.querySelector(".sidebar")
-    sidebar.classList.toggle("visible")
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("visible");
   };
 
   return (
-    <div className='navbar'>
-      {/* <span className="logo">Chat</span> */}
+    <div className="navbar">
       <div className="user">
-        <img src={currentUser.photoURL} alt="photos" />
-        <span>{currentUser.displayName}</span>
+        <img src={userPhoto} alt="photos" />
+        <span>{"Hardy"}</span>
       </div>
-      <button className='x' onClick={handleOpenMenu}>
+      <button className="x" onClick={handleOpenMenu}>
         <img src={Xmark} alt="" />
-        {/* X */}
       </button>
-      <button className='deconnexion' onClick={() => signOut(auth)}>Déconnexion</button>
+      <button className="deconnexion">
+        <FontAwesomeIcon icon={faSignOutAlt} flip style={{marginRight: '5px'}}/>
+        Déconnexion
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+// import React, { useContext } from 'react'
+// // import photos from "../img/add.png"
+// import { signOut } from 'firebase/auth'
+// import { auth } from '../firebase'
+// import { AuthContext } from '../context/AuthContext'
+// import Xmark from '../img/x.png'
+
+// const Navbar = () => {
+//   const {currentUser} = useContext(AuthContext)
+
+//   const handleOpenMenu = () => {
+//     const sidebar = document.querySelector(".sidebar")
+//     sidebar.classList.toggle("visible")
+//   };
+
+//   return (
+//     <div className='navbar'>
+//       {/* <span className="logo">Chat</span> */}
+//       <div className="user">
+//         <img src={currentUser.photoURL} alt="photos" />
+//         <span>{currentUser.displayName}</span>
+//       </div>
+//       <button className='x' onClick={handleOpenMenu}>
+//         <img src={Xmark} alt="" />
+//         {/* X */}
+//       </button>
+//       <button className='deconnexion' onClick={() => signOut(auth)}>Déconnexion</button>
+//     </div>
+//   )
+// }
+
+// export default Navbar
--- client/src/components/Messanger/components/Search.jsx
+++ client/src/components/Messanger/components/Search.jsx
@@ -1,127 +1,146 @@
-import React, { useContext, useState } from "react";
 import SearchIcon from "../img/loupe-arrondie.png";
-// import homme from "../img/avatar/homme.png";
-import { db } from "../firebase";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  setDoc,
-  doc,
-  updateDoc,
-  serverTimestamp,
-  getDoc,
-} from "firebase/firestore";
-import { AuthContext } from "../context/AuthContext";
-// import { ChatContext } from "../context/ChatContext";
+import homme from "../img/avatar/homme.png";
 
 const Search = () => {
-  const [username, setUsername] = useState("");
-  const [user, setUser] = useState(null);
-  const [err, setErr] = useState(false);
-
-  const { currentUser } = useContext(AuthContext);
-
-  // ================== Test ================
-  // const { dispatch } = useContext(ChatContext);
-
-
-  const handleSearch = async () => {
-    const q = query(
-      collection(db, "users"),
-      where("displayName", "==", username)
-    );
-
-    try {
-      const querySnapshot = await getDocs(q);
-      if (querySnapshot.empty) {
-        setErr(true);
-      } else {
-        querySnapshot.forEach((doc) => {
-          setUser(doc.data());
-        });
-        setErr(false); 
-      }
-
-    } catch (err) {
-      setErr(true)
-      console.log("utilisateur introuvable");
-    }
-
-  };
-
-
-  const handleKey = (e) => {
-    e.code === "Enter" && handleSearch();
-  };
-
-  const handleSelect = async (u) => {
-
-    const combinedId =
-      currentUser.uid > user.uid
-        ? currentUser.uid + user.uid
-        : user.uid + currentUser.uid;
-
-    try {
-      const res = await getDoc(doc(db, "chats", combinedId));
-
-      if (!res.exists()) {
-        await setDoc(doc(db, "chats", combinedId), { messages: [] });
-
-        await updateDoc(doc(db, "useChats", currentUser.uid), {
-          [combinedId + ".userInfo"]: {
-            uid: user.uid,
-            displayName: user.displayName,
-            photoURL: user.photoURL,
-          },
-          [combinedId + ".date"]: serverTimestamp(),
-        });
-
-        await updateDoc(doc(db, "useChats", user.uid), {
-          [combinedId + ".userInfo"]: {
-            uid: currentUser.uid,
-            displayName: currentUser.displayName,
-            photoURL: currentUser.photoURL,
-          },
-          [combinedId + ".date"]: serverTimestamp(),
-        });
-      }
-    } catch (err) {}
-
-    setUser(null);
-    setUsername("");
-    // dispatch({type: "CHANGE_USER", payload: u })
-
-  };
-
   return (
     <div className="search">
       <div className="searchForm">
         <div className="input">
-          <input
-            type="text"
-            placeholder="Rechercher client"
-            onKeyDown={handleKey}
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-          />
+          <input type="text" placeholder="Rechercher client" />
           <div className="iconRecherche">
-            <img src={SearchIcon} alt="" onClick={handleSearch} />
+            <img src={SearchIcon} alt="" />
           </div>
         </div>
-      {err && <span className="errorMessage">Client introuvable !!</span>}
+        <span className="errorMessage">Client introuvable !!</span>
       </div>
-      {user && (
-        <div className="userChat" onClick={handleSelect}>
-          <img src={user.photoURL} alt="" />
-          <div className="userChatInfo">
-            <span>{user.displayName}</span>
-          </div>
+      <div className="userChat">
+        <img src={homme} alt="" />
+        <div className="userChatInfo">
+          <span>{"Cedi"}</span>
         </div>
-      )}
+      </div>
     </div>
   );
 };
 
 export default Search;
+
+// import React, { useContext, useState } from "react";
+// import SearchIcon from "../img/loupe-arrondie.png";
+// import { db } from "../firebase";
+// import {
+//   collection,
+//   query,
+//   where,
+//   getDocs,
+//   setDoc,
+//   doc,
+//   updateDoc,
+//   serverTimestamp,
+//   getDoc,
+// } from "firebase/firestore";
+// import { AuthContext } from "../context/AuthContext";
+
+// const Search = () => {
+//   const [username, setUsername] = useState("");
+//   const [user, setUser] = useState(null);
+//   const [err, setErr] = useState(false);
+
+//   const { currentUser } = useContext(AuthContext);
+
+//   const handleSearch = async () => {
+//     const q = query(
+//       collection(db, "users"),
+//       where("displayName", "==", username)
+//     );
+
+//     try {
+//       const querySnapshot = await getDocs(q);
+//       if (querySnapshot.empty) {
+//         setErr(true);
+//       } else {
+//         querySnapshot.forEach((doc) => {
+//           setUser(doc.data());
+//         });
+//         setErr(false);
+//       }
+
+//     } catch (err) {
+//       setErr(true)
+//       console.log("utilisateur introuvable");
+//     }
+
+//   };
+
+//   const handleKey = (e) => {
+//     e.code === "Enter" && handleSearch();
+//   };
+
+//   const handleSelect = async (u) => {
+
+//     const combinedId =
+//       currentUser.uid > user.uid
+//         ? currentUser.uid + user.uid
+//         : user.uid + currentUser.uid;
+
+//     try {
+//       const res = await getDoc(doc(db, "chats", combinedId));
+
+//       if (!res.exists()) {
+//         await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+//         await updateDoc(doc(db, "useChats", currentUser.uid), {
+//           [combinedId + ".userInfo"]: {
+//             uid: user.uid,
+//             displayName: user.displayName,
+//             photoURL: user.photoURL,
+//           },
+//           [combinedId + ".date"]: serverTimestamp(),
+//         });
+
+//         await updateDoc(doc(db, "useChats", user.uid), {
+//           [combinedId + ".userInfo"]: {
+//             uid: currentUser.uid,
+//             displayName: currentUser.displayName,
+//             photoURL: currentUser.photoURL,
+//           },
+//           [combinedId + ".date"]: serverTimestamp(),
+//         });
+//       }
+//     } catch (err) {}
+
+//     setUser(null);
+//     setUsername("");
+
+//   };
+
+//   return (
+//     <div className="search">
+//       <div className="searchForm">
+//         <div className="input">
+//           <input
+//             type="text"
+//             placeholder="Rechercher client"
+//             onKeyDown={handleKey}
+//             onChange={(e) => setUsername(e.target.value)}
+//             value={username}
+//           />
+//           <div className="iconRecherche">
+//             <img src={SearchIcon} alt="" onClick={handleSearch} />
+//           </div>
+//         </div>
+//       {err && <span className="errorMessage">Client introuvable !!</span>}
+//       </div>
+//       {user && (
+//         <div className="userChat" onClick={handleSelect}>
+//           <img src={user.photoURL} alt="" />
+//           <div className="userChatInfo">
+//             <span>{user.displayName}</span>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Search;
