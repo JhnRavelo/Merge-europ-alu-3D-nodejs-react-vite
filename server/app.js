1ca7521c601c8255ddf6897d41e8b89599b4a6a7
JhnRavelo <johnravelo135@gmail.com>
2023-10-20 16:40:26 +0300

    server port

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://192.168.123.210:5173"],
+    origin: ["http://localhost:5173", "http://192.168.123.210:5173", "http://192.168.137.1:5173"],
     methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
   },
 });
@@ -59,7 +59,7 @@ users.hasOne(sessions, { foreignKey: "userId" });
 sessions.belongsTo(users, { foreignKey: "userId" });
 
 db.sequelize.sync().then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT, "0.0.0.0",() => {
     console.log(`http://127.0.0.1:${process.env.PORT}`);
   });
 });
@@ -71,7 +71,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     credentials: true,
-    origin: [`http://localhost:5173`, "http://192.168.123.210:5173"],
+    origin: [`http://localhost:5173`, "http://192.168.123.210:5173", "http://192.168.137.1:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
   })
 );
